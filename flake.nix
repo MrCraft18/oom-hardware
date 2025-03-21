de0e32752601385bec5552df497ab64f3815cfd7
MrCraft18 <mariocaden@gmail.com>
2025-03-21 01:56:45 -0500

    redadded proper flake inputs?

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
-  description = "oom-hardware";
+    description = "oom-hardware";
 
-  outputs = {...}: {
-    nixosModules = {
-      uconsole = import ./uconsole;
-      deskpi = import ./deskpi;
-      cm4 = import ./cm4;
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    outputs = { nixpkgs, nixos-hardware, ... }: 
+    let
+        system = "aarch64-linux";
+        pkgs = import nixpkgs {inherit system;};
+    in {
+        nixosModules = {
+            uconsole = import ./uconsole {inherit pkgs nixos-hardware;};
+            deskpi = import ./deskpi {inherit pkgs nixos-hardware;};
+            cm4 = import ./cm4;
+        };
     };
-  };
 }
